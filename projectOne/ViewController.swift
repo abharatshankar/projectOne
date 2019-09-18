ae1338337f0d92b6f6e17f429bb87e43e89e7501
bharat <bharatshankar5>
2019-09-19 01:13:06 +0530

    all commits finished

---
+++ projectOne/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  projectOne
+//
+//  Created by Bharat shankar on 19/09/19.
+//  Copyright © 2019 Bharat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
